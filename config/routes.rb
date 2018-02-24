2f0bcdf8216fa82b5624cb9e03528c155c5fa869
HBB Solutions <ios.hbbsolutions@gmail.com>
2018-02-24 16:29:28 +0700

    block login

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
 		resources :comments
 	end
 
-	get	'/login' to "sessions#new"
-	post '/login' to 'sessions#create'
-	delete '/login' to 'sessions#destroy'
+	# get	'/login' to "sessions#new"
+	# post '/login' to 'sessions#create'
+	# delete '/login' to 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

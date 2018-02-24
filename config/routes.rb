cd1c113042cfad0e75eff4d92ba7b991ee761201
HBB Solutions <ios.hbbsolutions@gmail.com>
2018-02-24 16:20:41 +0700

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
 	get 'welcome/index'
 	root 'welcome#index'
 	resources :articles do
 		resources :comments
 	end
+
+	get	'/login' to "sessions#new"
+	post '/login' to 'sessions#create'
+	delete '/login' to 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

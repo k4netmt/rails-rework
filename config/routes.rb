2e27818735b8a96a9cce40c779e438db90eef0b7
HBB Solutions <ios.hbbsolutions@gmail.com>
2018-03-02 23:16:40 +0700

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
 	get     "login"    => "sessions#new"
 	post "login" => "sessions#create"
 	delete  "logout"   => "sessions#destroy"
-	delete 'articles/destroy'
 	resources :users
 	# post '/login' to 'sessions#create'
 	# delete '/login' to 'sessions#destroy'
